800176adc504c58c1e29f48f868903ff53409966
Yogita Bhargava <79744488+yogitabhargava02@users.noreply.github.com>
2023-10-02 23:31:39 +0530

    changes

--- src/components/CardPage.js
+++ src/components/CardPage.js
@@ -1,22 +1,54 @@
 import React, { useState } from 'react';
 import cardData from './Cards.json';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
 
 const CardPage = () => {
   const [cars, setCars] = useState(cardData.cars);
 
   return (
-    <div className="container mx-auto py-8 bg-blue">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+    <div className="container mx-auto py-6 bg-blue">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
         {cars.map((car) => (
-          <div key={car.car_id} className="card">
-            <img src={car.car_image_url} alt={car.car_title} />
-            <h2>{car.car_title}</h2>
-            <p>Year: {car.year_manufactured}</p>
-            <p>Seating Capacity: {car.seating_capacity}</p>
-            <p>Fuel Type: {car.fuel_type}</p>
-            <p>Transmission Type: {car.transmission_type}</p>
-            <p>Mileage: {car.mileage}</p>
-            <p>Price per Month: ${car.price_per_month}</p>
+          <div key={car.car_id} className="card rounded-3xl" style={{ backgroundColor: "#F0F8FF", boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)", height:"98%"}}>
+            <img src={car.car_image_url} alt={car.car_title} className="rounded-3xl p-4" />
+
+            <div class="flex justify-between">
+              <div className='pl-4 text-3xl'>
+                <h2>{car.car_title}</h2>
+              </div>
+              <div className='flex p-2 pr-4'>
+                <p className='border-dashed border-2 border-indigo-600 rounded-xl p-1 w-50'>{car.year_manufactured}</p>
+              </div>
+            </div>
+
+            <div class='flex justify-between p-6 pb-2 pt-2 '>
+              <div class='flex flex-col'>
+                <p className="mr-12 pb-6">{car.seating_capacity} People</p>
+                <p>{car.fuel_type}</p>
+              </div>
+              <div class='flex flex-col'>
+                <p className='pb-6'>{car.mileage}km/1-litre</p>
+                <p className="mr-4">{car.transmission_type}</p>
+              </div>
+            </div>
+
+            <div className='flex flex-row justify-between pl-4 pr-1 pb-4'>
+            <div className='text-lg flex-end'>
+            <p className='text-lg p-4'>{car.price_per_month}</p>
+            </div>
+              
+                
+                <div className='flex justify-between p-2'>
+                <FontAwesomeIcon icon={farHeart} className='text-3xl ml-2 pr-2 pt-2' />
+                <button className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-1 px-3 rounded-full shadow-md pb-2 pl-2'>
+  Rent now
+</button>
+
+                </div>
+               
+              </div>
+            
           </div>
         ))}
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,51 @@
-import React from 'react'
+import React from 'react';
 import 'tailwindcss/tailwind.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown, faSearch } from '@fortawesome/free-solid-svg-icons'; // Import the specific icon you need
 import CardPage from './CardPage';
+
 const Home = () => {
   return (
     <div>
-<div className='flex justify-space p-14'>
-<input
-   type="text"
-   placeholder="Search here"
-  
-  />
-  <h2>Relevance </h2>
-  <h2>All brands</h2>
-</div>
-      <div className="container w-full mx-auto bg-blue p-5">
+      <div className='flex p-6 m-10 rounded-sm' style={{ backgroundColor: "#F0F8FF", boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)" }}>
 
-<CardPage/>
 
-      <div class="grid grid-flow-col justify-stretch ...">
- 
-</div>
+
+      
+      <div className='relative w-15'>
+          
+          <input
+            type="text"
+            placeholder="Search here"
+            className='text-lg p-1 pl-10 w-13 border rounded focus:outline-none focus:border-blue-500'
+          />
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            <FontAwesomeIcon icon={faSearch} className='text-gray-400' /> {/* Use the correct imported icon */}
+          </div>
+        </div>
+      
+        <div className='mr-19 flex'>
+          <h2>Relevance</h2>
+          <FontAwesomeIcon icon={faAngleDown} className='p-1.5'/> {/* Use the imported icon */}
+        </div>
+        <div className='mr-1 flex'>
+          <h2>All Brands</h2>
+          <FontAwesomeIcon icon={faAngleDown} className='p-1.5' /> {/* Use the imported icon */}
+        </div>
       </div>
+      <div className="container w-full mx-auto bg-blue p-6">
+        <CardPage />
+        <div className="grid grid-flow-col justify-stretch ..."></div>
+      </div>
+      <div className='flex p-6 m-10 rounded-sm' style={{ backgroundColor: "#F0F8FF", boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)" }}>
+
+
+</div>
     </div>
-  )
+
+
+ 
+  );
 }
 
-export default Home
+export default Home;
